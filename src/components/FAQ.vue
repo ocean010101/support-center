84fd21f45a1258a01900f26eb613ad55ff90fed0
ocean010101 <yangyang01.xu@outlook.com>
2019-09-24 22:17:24 +0800

    Add $fetch method
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,7 +1,6 @@
 <template>
     <main class="faq">
         <h1>Frequently Asked Questions</h1>
-
          <Loading v-if="loading" />
 
         <div class="error" v-if="error">
@@ -25,24 +24,14 @@ export default {
             loading: false
         }
     },
-    created() {
+    async created() {
         this.loading = true;
-        fetch('http://localhost:3000/questions')
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
-                } else {
-                    return Promise.reject('error')
-                }
-            })
-            .then(result => {
-                this.questions = result;
-                this.loading = false;
-            })
-            .catch(e => {
-                this.error = e;
-                this.loading = false;
-            })
+        try {
+            this.questions = await this.$fetch('questions')
+        } catch (e) {
+            this.error = e
+        }
+        this.loading = false;
     }
 }
 </script>
--- src/plugins/fetch.js
+++ src/plugins/fetch.js
@@ -1,8 +1,20 @@
 let baseUrl;
 
+export async function $fetch(url) {
+    const response = await fetch(`${baseUrl}${url}`)
+    if (response.ok) {
+        const data = await response.json()
+        return data;
+    } else {
+        const error = new Error(message)
+        throw error;
+    }
+}
+
 export default {
     install(Vue, options) {
         baseUrl = options.baseUrl;
         console.log('Installed', options);
+        Vue.prototype.$fetch = $fetch
     }
 }
