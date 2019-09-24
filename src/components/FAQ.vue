1477616a8ed27a98a91f64341ea23396a5e28545
ocean010101 <yangyang01.xu@outlook.com>
2019-09-24 21:02:08 +0800

    Create router
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ src/components/FAQ.vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="faq">
+        <h1>Frequently Asked Questions</h1>
+    </main>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import 'babel-polyfill'
 import Vue from 'vue'
+import AppLayout from './components/AppLayout.vue'
+import router from './router'
 
 new Vue({
   el: '#app',
-  render: h => h('div', 'Support Center')
+  render: h => h(AppLayout),
+  router,
 })
