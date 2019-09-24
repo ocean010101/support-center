d7e68a362871c607198adf3724d06a6f5ac29c02
ocean010101 <yangyang01.xu@outlook.com>
2019-09-24 21:53:01 +0800

    Fetch questions from server
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,5 +1,41 @@
 <template>
     <main class="faq">
         <h1>Frequently Asked Questions</h1>
+        <div class="error" v-if="error">
+            Can't load the questions
+        </div>
+
+        <section class="list">
+            <article v-for="question of questions" :key="question.id">
+                <h2 v-html="question.title"></h2>
+                <p v-html="question.content"></p>
+            </article>
+        </section>
     </main>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            questions: [],
+            error: null,
+        }
+    },
+    created() {
+        fetch('http://localhost:3000/questions')
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    return Promise.reject('error')
+                }
+            })
+            .then(result => {
+                this.questions = result;
+            })
+            .catch(e => {
+                this.error = e;
+            })
+    }
+}
+</script>
