29629c2c9bed93c133cd19512c0c5cb02eff70b6
ocean010101 <yangyang01.xu@outlook.com>
2019-09-25 21:29:40 +0800

    Update Login component
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,30 +4,31 @@
         <SmartForm class="form" :title="title" :operation="operation" :valid="valid">
             <FormInput name="username" v-model="username" placeholder="Username" />
             <FormInput name="password" type="password" v-model="password" placeholder="Password" />
+
+            <template v-if="mode === 'signup'">
+                <FormInput name="verify-password" type="password" v-model="password2" placeholder="Retype Password"
+                    :invalid="retypePasswordError" />
+                <FormInput name="email" type="email" v-model="email" placeholder="Email" />
+            </template>
+            <template slot="actions">
+                <template v-if="mode === 'login'">
+                    <button type="button" class="secondary" @click="mode = 'signup'">
+                        Sign up
+                    </button>
+                    <button type="submit" :disabled="!valid">
+                        Login
+                    </button>
+                </template>
+                <template v-else-if="mode === 'signup'">
+                    <button type="button" class="secondary" @click="mode = 'login'">
+                        Back to login
+                    </button>
+                    <button type="submit" :disabled="!valid">
+                        Create account
+                    </button>
+                </template>
+            </template>
         </SmartForm>
-        <template v-if="mode === 'signup'">
-            <FormInput name="verify-password" type="password" v-model="password2" placeholder="Retype Password"
-                :invalid="retypePasswordError" />
-            <FormInput name="email" type="email" v-model="email" placeholder="Email" />
-        </template>
-        <template slot="actions">
-            <template v-if="mode === 'login'">
-                <button type="button" class="secondary" @click="mode = 'signup'">
-                    Sign up
-                </button>
-                <button type="submit" :disabled="!valid">
-                    Login
-                </button>
-            </template>
-            <template v-else-if="mode === 'signup'">
-                <button type="button" class="secondary" @click="mode = 'login'">
-                    Back to login
-                </button>
-                <button type="submit" :disabled="!valid">
-                    Create account
-                </button>
-            </template>
-        </template>
     </main>
 </template>
 <script>
