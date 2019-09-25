197a281e5a9ee24c03f5f40b8a427fa8807b53f3
ocean010101 <yangyang01.xu@outlook.com>
2019-09-25 21:45:25 +0800

    Add "Route Navigation Guard"
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from './components/Home.vue'
 import FAQ from './components/FAQ.vue'
 import Login from './components/Login.vue'
 import TicketsLayout from './components/TicketsLayout.vue'
+import state from './state'
 
 Vue.use(VueRouter)
 
@@ -11,11 +12,20 @@ const routes = [
     { path: '/', name: 'home', component: Home },
     { path: '/faq', name: 'faq', component: FAQ },
     { path: '/login', name: 'login', component: Login },
-    { path: '/tickets', name: 'tickets', component: TicketsLayout },
+    { path: '/tickets', name: 'tickets', component: TicketsLayout, meta: { private: true } },
 ]
 
 const router = new VueRouter({
     routes,
     mode: 'history'
+})
+
+router.beforeEach((to, from, next) => {
+    console.log('to=', to.name);
+    if (to.meta.private && !state.user) {
+        next({ name: 'home' })
+        return
+    }
+    next()
 })
 export default router
